87112355cdcb3f29222bdb994a8b0e7405babe35
Pyojin Kim <pjinkim1215@gmail.com>
2022-02-10 03:32:01 +0900

    add read images and text files.

--- visualization/main_script.m
+++ visualization/main_script.m
@@ -4,8 +4,12 @@ clear variables; %clear classes;
 rand('state',0); % rand('state',sum(100*clock));
 dbstop if error;
 
+addpath('jsonlab-2.0');
+addpath('lsd_1.6');
+addpath(genpath(pwd));
 
-%% common setting to read text files
+
+%% common setting to read 3D Scanner App data
 
 %
 datasetPath = '../renaissance_plaza_scan/';
@@ -15,52 +19,70 @@ frameIndex =  831;
 % read color, depth, and confidence image
 colorImageFileName = [datasetPath sprintf('frame_%05d.jpg', frameIndex)];
 depthImageFileName = [datasetPath sprintf('depth_%05d.png', frameIndex)];
-confImageFileName = [datasetPath sprintf('conf_%05d.jpg', frameIndex)];
+confImageFileName = [datasetPath sprintf('conf_%05d.png', frameIndex)];
 
 jsonTextFileName = [datasetPath sprintf('frame_%05d.json', frameIndex)];
 
 
 %%
 
-
+% iOS camera parameters
 value = loadjson(jsonTextFileName);
 
 
-
-
-
-colorImage = imread('frame_00831.jpg');
-grayImage = rgb2gray(colorImage).';
-grayImage = flip(grayImage,2);
-grayImageResized = imresize(grayImage,[256 192]);
-
-depthImage = imread('depth_00831.png').';
-depthImage = flip(depthImage,2);
+% color image
+colorImage = imread(colorImageFileName);
+colorImage = imrotate(colorImage,-90);
+grayImage = rgb2gray(colorImage);
 
 
 figure;
-imshow(grayImage,[]);
+imshow(colorImage,[]);
 
 
-figure;
-imshow(grayImageResized,[]);
+% depth image
+depthImage = imread(depthImageFileName);
+depthImage = imrotate(depthImage,-90);
+
 
 figure;
 imshow(depthImage,[]);
 
+
+% confidence image
+confImage = imread(confImageFileName);
+confImage = imrotate(confImage,-90);
+
+
+figure;
+imshow(confImage,[]);
+
+
+%%
+
+
+colorImageResized = imresize(colorImage,[256 192]);
+
+figure;
+imshow(colorImageResized,[]);
 
 
 %%
 
 1024
 
-
+grayImageResized = imresize(grayImage,[256 192]);
 
 1920 1440
-
-
-
 256 192
+
+
+
+
+
+
+
+
 
 
 
--- visualization/test_code.m
+++ visualization/test_code.m
@@ -2,6 +2,74 @@
 
 
 
+fx = value.intrinsics(1);
+fy = value.intrinsics(5);
+cx = value.intrinsics(6);
+cy = value.intrinsics(3);
+
+cam.K = eye(3);
+cam.K(1,1) = fx;
+cam.K(1,3) = cx;
+cam.K(2,2) = fy;
+cam.K(2,3) = cy;
+
+cam.K(1,:) = cam.K(1,:) / (1440/192);
+cam.K(2,:) = cam.K(2,:) / (1920/256);
+
+
+cam.K
+
+
+points = zeros(3,256*192);
+colors = zeros(3,256*192);
+count = 0;
+for v = 1:256
+    for u = 1:192
+        count = count + 1;
+        points(:,count) = (inv(cam.K) * [u;v;1]) * (double(depthImage(v,u)) / 1000);
+        colors(:,count) = squeeze(colorImageResized(v,u,:));
+    end
+end
+
+
+figure;
+scatter3(points(1,:), points(2,:), points(3,:),30*ones(1,49152),(colors/255).'); axis equal;
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+
+
+% line detection
+imageCurForLine = grayImage;
+lineLength = 150;
+
+dimageCurForLine = double(imageCurForLine);
+[lines, ~] = lsdf(dimageCurForLine, (lineLength^2));
+% lines = extractUniqueLines(imageCurForLine, lines, cam);
+
+
+% plot image with lines / Gaussian sphere with line normals, vDD
+figure;
+imshow(imageCurForLine, []); hold on;
+for i = 1:size(lines,1)
+    plot([lines(i,1),lines(i,3)],[lines(i,2),lines(i,4)],'LineWidth',2.5);
+end
+
+
+%%
+
+
+
 filename = 'Untitled_Scan_19_25_13.txt';
 textData = importdata(filename, ',');
 
@@ -27,3 +95,5 @@ temp = imread('../Untitled_Scan_19_27_01_All_Data/2022_02_07_14_03_31/conf_00294
 
 figure;
 imshow(temp,[]);
+
+
