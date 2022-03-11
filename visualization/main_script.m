dce09c7688d5193cccfde4c329f3db34b029b373
Pyojin Kim <pjinkim1215@gmail.com>
2022-03-12 03:07:08 +0900

    add result image file and some minor changes.

--- visualization/main_script.m
+++ visualization/main_script.m
@@ -21,7 +21,7 @@ datasetPath = '../rgbd_dataset_renaissance_03_corridor01/';
 
 % plot 3D estimated trajectory with 3-D reconstruction results
 figure;
-set(gcf,'color','w'); hold on; axis equal; axis off; view(0,0);
+set(gcf,'color','w'); hold on; axis equal; axis on;
 set(gcf,'Units','pixels','Position',[500 20 1400 1000]);
 
 % draw 3-D reconstruction results
@@ -29,7 +29,7 @@ numPoints = length(ARKitPointsXYZ);
 scatter3(ARKitPointsXYZ(1,:), ARKitPointsXYZ(2,:), ARKitPointsXYZ(3,:), 100*ones(numPoints,1), (ARKitPointsRGB ./ 255).','.');
 
 % draw camera frame
-L = 0.5; % coordinate axis length
+L = 1.0; % coordinate axis length
 A = [0 0 0 1; L 0 0 1; 0 0 0 1; 0 L 0 1; 0 0 0 1; 0 0 L 1]';
 for k = 1:10:length(T_gc_ARKit)
     
@@ -41,12 +41,12 @@ for k = 1:10:length(T_gc_ARKit)
     plot3(B(1,5:6),B(2,5:6),B(3,5:6),'-b','LineWidth',1);   % z: blue
     
     % camera frame frustum
-    plot_camera_frame_black_only(T_gc_ARKit{k}(1:3,1:3), T_gc_ARKit{k}(1:3,4), 1.0, 'k');
+    plot_camera_frame_black_only(T_gc_ARKit{k}(1:3,1:3), T_gc_ARKit{k}(1:3,4), 2.0, 'k');
 end
 
 % draw ARKit 3D trajectory
 plot3(stateEsti_ARKit(1,:),stateEsti_ARKit(2,:),stateEsti_ARKit(3,:),'r','LineWidth',2.5);
-plot_inertial_frame(5.0); axis equal; axis on; grid on; view(0,0);
+plot_inertial_frame(5.0); axis equal; axis on; grid on;
 xlabel('X [m]'); ylabel('Y [m]'); zlabel('Z [m]');
 
 % figure options
